a3f7363010a88682512c1f51f45811cb6dd04b28
thong <2446871238@qq.com>
2020-06-29 18:22:26 +0800

    第一次提交

---
+++ h5/js/h5.js
@@ -0,0 +1,24 @@
+// function displayDate(){
+// 	document.getElementById("demo").innerHTML=Date();
+// }
+
+// function myFunction1(){
+// 	var carname="Volvo";
+// 	document.getElementById("demo").innerHTML=carname;
+// }
+// function hello(){
+// 	var x;
+// 	var person=prompt("请输入你的名字","Harry Potter");
+// 	if (person!=null && person!=""){
+// 	    x="你好 " + person + "! 今天感觉如何?";
+// 	    document.getElementById("demo").innerHTML=x;
+// 	}
+// }
+// function math(a,b){
+// 	return a*b;
+// }
+
+// var a = 4;
+// function myFunction() {
+// 	document.getElementById("demo").innerHTML = a * a;
+// 	}
