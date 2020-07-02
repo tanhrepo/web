0d34ec5fe10eed8c34177f24a027a1d17714bb00
thong <2446871238@qq.com>
2020-07-03 01:38:57 +0800

    bootstrap阴阳师

--- h24/js/h24.js
+++ h24/js/h24.js
@@ -2,3 +2,10 @@ $("#mytab a").click(function(e){
 	e.preventDefault();
 	$(this).tab("show");
 })
+// $("#damu-nav li a").click(function(e){
+// 	var href = $(this).attr("href");
+// 	console.log("#damu-list > li > a[href="+(href)+"]");
+// 	$("#damu-list > li > a[href="'+(href)+'"]").tab('show');
+// 	$(document).scrollTop($("#demo").offset().top);
+// 	e.preventDefault();
+// })
